f301062c20f853451eef715a6248228c7ff29f92
Mayank Kumar <mayank22oct@gmail.com>
2021-07-25 04:32:43 +0530

    feat: added info message while initalizing sentry

--- pkg/sentryUtil/sentry.go
+++ pkg/sentryUtil/sentry.go
@@ -1,6 +1,7 @@
 package sentryUtil
 
 import (
+	"fmt"
 	"github.com/getsentry/sentry-go"
 	"github.com/sirupsen/logrus"
 	"os"
@@ -8,6 +9,7 @@ import (
 
 func InitSentry()  {
 	sentryUrl := os.Getenv("SENTRY_URL")
+	fmt.Println("[INFO] initializing sentry:", sentryUrl)
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn: sentryUrl,
 	})
