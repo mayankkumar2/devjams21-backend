550ffaea58ac725429c65edd57ae6311eb7b8b50
Mayank Kumar <mayank22oct@gmail.com>
2021-10-26 21:39:35 +0530

    fix: freeze

--- pkg/model/submission_model.go
+++ pkg/model/submission_model.go
@@ -2,6 +2,6 @@ package model
 
 type Submission struct {
 	BaseModel
-	Freeze bool `json:"-" gorm:"default:false"`
+	Freeze bool `json:"freeze" gorm:"default:false"`
 	Meta   JSON `json:"meta" gorm:"type:json"`
 }
